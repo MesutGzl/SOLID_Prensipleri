350e8592b8ea688e8beffb19ae85d819ab38a201
Mesut Güzel <71979276+MesutGzl@users.noreply.github.com>
2021-04-10 16:49:10 +0300

    DIP

---
+++ 5-DIP/BadExample/EmployeeDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIP.BadExample
+{
+    public class EmployeeDetails
+    {
+        public int HoursWorked { get; set; }
+        public int HourlyRate { get; set; }
+
+        public float GetSalary(){
+            SalaryCalculator salaryCalculator = new SalaryCalculator();
+            return salaryCalculator.CalculateSalary(HoursWorked, HourlyRate);
+        }
+    }
+}
